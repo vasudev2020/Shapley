b5e92ad270d2d6f2fd209af1da3af28099458710
Vasudevan Nedumpozhimana <vasudevan.nedumpozhimana@adaptcentre.ie>
2021-05-12 17:18:20 +0100

    Updated

---
+++ GITI.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  6 10:56:26 2021
+
+@author: vasu
+"""
+
+#from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix,roc_auc_score
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.neural_network import MLPClassifier
+from scipy import stats
+import warnings
+from tqdm import tqdm
+from warnings import filterwarnings
+from sklearn.exceptions import ConvergenceWarning
+
+import random
+#from collections import defaultdict
+from statistics import mean
+
+
+
+from Embedding import Embedding
+
+
+class GITI:   
+    def __init__(self,Data,Labels,allembs=False):
+        self.num_folds = 5
+        self.emb = Embedding(allembs)        
+        self.Embs = {}
+        self.CreateFolds(Data,Labels)
+        self.mlp = MLPClassifier()
+        self.exps = list(Data.keys())
+        self.t = 2
+         
+    def CreateFolds(self, Data,Labels):
+        skf = StratifiedKFold(n_splits=self.num_folds)
+        
+        self.XFolds,self.YFolds = {},{}
+        
+        for exp in Data:
+            if exp not in self.XFolds:  self.XFolds[exp] = [[],[],[],[],[]] 
+            if exp not in self.YFolds:  self.YFolds[exp] = [[],[],[],[],[]] 
+            
+            for fi, [train_index, test_index] in enumerate(skf.split(Data[exp], Labels[exp])):
+                for ti in test_index:
+                    self.XFolds[exp][fi].append(Data[exp][ti])
+                    self.YFolds[exp][fi].append(Labels[exp][ti])         
+                       
+    def Evaluate(self,tr_exps,ts_exps):
+        auc = []
+        for fold in range(5):
+            trainX = [self.getEmb(sent,'BERT11') for f in range(5) for exp in tr_exps for sent in self.XFolds[exp][f]   if f!=fold]
+            trainY = [l for f in range(5) for exp in tr_exps for l in self.YFolds[exp][f] if f!=fold]
+                
+            testX = [self.getEmb(sent,'BERT11') for exp in ts_exps for sent in self.XFolds[exp][fold]]
+            testY = [l for exp in ts_exps for l in self.YFolds[exp][fold]]
+               
+            with warnings.catch_warnings():
+                filterwarnings("ignore", category=ConvergenceWarning)
+                self.mlp.fit(trainX,trainY)
+                
+            pred_proba = [proba[1] for proba in self.mlp.predict_proba(testX)]
+            auc.append(roc_auc_score(testY,pred_proba))
+        return mean(auc)
+            
+        
+    def getEmb(self,sent,embtype):
+        if sent not in self.Embs:   self.Embs[sent] = self.emb.getMean(sent)
+        return self.Embs[sent][embtype]
+    
+    def getSE(self):
+        print('SE')
+        for exp in self.exps:   print(exp,self.Evaluate([exp],self.exps))
+            
+    def getLOO(self):
+        print('LOO')
+        for exp in self.exps:   print(exp,self.Evaluate(list(set(self.exps)-set([exp])),self.exps))
+        
+    def getDifficulty(self):
+        print('Difficulty')
+        for exp in self.exps:   print(exp,self.Evaluate(self.exps,[exp]))
+        
+    '''return shapley values of idiomatic expressions '''
+    def getShapleyValues(self):
+        SV = {}
+        for exp in tqdm(range(len(self.exps))):
+            V = 0.0
+            for i in range(len(self.exps)):
+                for _ in range(self.t):
+                    p = list(range(len(self.exps)))
+                    random.shuffle(p)
+                    j = p.index(exp)
+                    p[i],p[j] = p[j],p[i]
+
+                    #self.train([self.idioms[j] for j in p[:i+1]])
+                    #V += self.test(self.idioms)[2]
+                    
+                    V += self.Evaluate([self.exps[j] for j in p[:i+1]],self.exps)
+                    if i==0:    continue
+                    #self.train([self.idioms[j] for j in p[:i]])
+                    #V -= self.test(self.idioms)[2]
+                    V -= self.Evaluate([self.exps[j] for j in p[:i]],self.exps)
+            SV[self.exps[exp]] = V / (len(self.exps)*self.t)
+            #sh.append(V / (len(self.exps)*self.t))
+        return SV
+    
+    def getUnseenExpResults(self,LO,SE,SV):
+        #LO = ['kick heel','have word','pull leg','make pile','blow whistle','get wind','make hay','blow trumpet','make face','hit road','pull punch','blow top','cut figure','get sack','get nod','see star','hit wall','hold fire','make hit','make scene','hit roof','pull plug','take heart','pull weight','make mark','lose thread','find foot','lose head']
+        #SE = ['pull plug','get wind','blow trumpet','hit road','kick heel','pull weight','blow whistle','hit roof','take heart','make pile','make hay','make mark','get sack','pull punch','blow top','cut figure','have word','lose thread','find foot','get nod','pull leg','make scene','make face','hold fire','lose head','hit wall','make hit','see star']
+        #SV = ['make mark','blow whistle','pull plug','blow trumpet','pull weight','make hay','hit roof','get wind','take heart','hit road','make pile','get sack','find foot','kick heel','make face','make scene','pull punch','blow top','lose thread','get nod','cut figure','pull leg','have word','hold fire','lose head','hit wall','make hit','see star']
+
+        LO_score = [0.0]*20
+        SE_score = [0.0]*20
+        SV_score = [0.0]*20
+        RO_score = [0.0]*20
+        
+        trials = 2
+        p = list(range(len(self.exps)))
+        for _ in range(trials):
+            random.shuffle(p)
+            for fold in range(5):
+                test_exps = [self.exps[i] for i in p[fold*5:(fold+1)*5]]
+                train_exps = [self.exps[i] for i in p[:fold*5]+p[(fold+1)*5:]]
+                lo_train_exps = [exp for exp in LO if exp in train_exps]
+                se_train_exps = [exp for exp in SE if exp in train_exps]
+                sv_train_exps = [exp for exp in SV if exp in train_exps]
+                
+                for i in range(20): LO_score[i]+=self.Evaluate(lo_train_exps[:i+1],test_exps)
+                for i in range(20): SE_score[i]+=self.Evaluate(se_train_exps[:i+1],test_exps)
+                for i in range(20): SV_score[i]+=self.Evaluate(sv_train_exps[:i+1],test_exps)
+                for i in range(20):    RO_score[i]+=self.Evaluate(train_exps[:i+1],test_exps)
+                
+        for i in range(20):
+            LO_score[i]/=trials*5
+            SE_score[i]/=trials*5
+            SV_score[i]/=trials*5
+            RO_score[i]/=trials*5
+            
+        print('LO:',LO_score)
+        print('SE:',SE_score)
+        print('SV:',SV_score)
+        print('RO:',RO_score)
+                
+    
+        
+        
---
+++ TopicDistSim.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 12 13:20:17 2021
+
+@author: vasu
+"""
+
+from collections import defaultdict
+#import spacy
+from ShapleyAnalyser import ShapleyAnalyser
+from TopicModel import TopicModel
+from statistics import mean
+from scipy.stats import pearsonr,spearmanr,entropy
+from tqdm import tqdm
+from scipy.special import rel_entr
+
+
+def LoadVNC(cutoff):        
+    with open("../Data/ID/cook_dataset.tok", "r") as fp:
+        dataset = fp.readlines()
+    dataset = [d.strip().split('||') for d in dataset]
+    dataset = [[1 if d[0]=='I' else 0, d[1]+' '+d[2], d[3].replace(' &apos;','\'').lower()] for d in dataset if d[0]!='Q']
+    
+    Datadict = defaultdict(lambda: defaultdict(list))
+    
+    Data = defaultdict(list)
+    Labels = defaultdict(list)
+    
+    for label,exp,sent in dataset:  Datadict[exp][label].append(sent)
+
+    print('Total number of expressions:',len(Datadict))
+    
+    for exp in Datadict.keys():    
+        if len(Datadict[exp][0])>=cutoff and len(Datadict[exp][1])>=cutoff:
+            for sent in Datadict[exp][0]:   
+                Data[exp].append(sent)
+                Labels[exp].append(0)
+            for sent in Datadict[exp][1]:   
+                Data[exp].append(sent)
+                Labels[exp].append(1)
+                  
+    print('Number of expressions after filtering:',len(Data))
+    
+    assert len(Data)==len(Labels)
+
+    return Data,Labels
+
+def LoadVNCTexts():
+    with open("../Data/ID/cook_dataset.tok", "r") as fp:
+        dataset = fp.readlines()
+    dataset = [d.strip().split('||')[3].replace(' &apos;','\'').lower() for d in dataset]
+    return dataset
+
+
+traintext = LoadVNCTexts()
+Data,Labels = LoadVNC(5) 
+TM = TopicModel()
+exps = [exp for exp in Data]
+
+
+def getTopicDistSim(T):
+    TM.train(traintext,T)
+    
+    testtext = [sent for exp in Data for sent in Data[exp]]
+    topics = TM.topicModel(testtext)
+    topic_dist = [0]*T
+    for t in topics:    topic_dist[t]+=1
+    topic_dist = [a/sum(topic_dist) for a in topic_dist]
+
+    dist_sim = []
+    for exp in exps:
+        #exp_topics = TM.topicModel([sent for sent in Data[exp]])
+        exp_topics = TM.topicModel([sent for sent,lb in zip(Data[exp],Labels[exp]) if lb==0])
+        exp_topic_dist = [0]*T
+        for t in exp_topics:    exp_topic_dist[t]+=1
+        exp_topic_dist = [a/sum(exp_topic_dist) for a in exp_topic_dist]
+
+        kld = sum(rel_entr(exp_topic_dist,topic_dist))
+        dist_sim.append(kld)
+    return dist_sim
+
+def getCorr(a,b):
+    assert len(a)==len(b)
+    return round(pearsonr(a,b)[0],4), round(spearmanr(a,b)[0],4)
+
+sv = [0.0293,0.0419,0.0534,0.0346,0.0374,0.0283,0.0342,0.0335,0.0365,0.0418,0.0458,0.0401,0.0349,0.0209,0.0288,0.0345,0.0338,0.0436,0.0346,0.0280,0.0353,0.0466,0.0449,0.0201,0.0368,0.0359]
+
+#for T in range(10,100,10):
+#    print(getCorr(sv,getTopicDistSim(T)))
+td =[getTopicDistSim(T) for T in range(10,100,10)]
+mtd = [mean(a) for a in zip(*td)]
+print(mtd)
+print(getCorr(sv,mtd))
